9d7c6e1d01baf476598f2b8cc2cac9712cc29b37
Lenovo <Lenovo@LAPTOP-RVCJ29LR>
2022-03-24 09:45:39 +0530

    This is my 1st commit

---
+++ src/com/git/export/NewTestGitDemo.java
@@ -0,0 +1,10 @@
+package com.git.export;
+
+public class NewTestGitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my 1st git export!!");
+	}
+
+}
